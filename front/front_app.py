13c59ba2b99c0e5050f18943c85bd0c2a158e62e
Vildan <brown.007@mail.ru>
2024-05-10 18:14:56 +0300

    add front

---
+++ front/front_app.py
@@ -0,0 +1,53 @@
+import gradio as gr
+import requests
+import json
+import os
+
+def predict_answer(question: str, context: str):
+    
+    input = {'question': question, 'context': context}
+    response = requests.post(
+        "http://web/api/predict",
+        data = json.dumps(input),
+        )
+    
+    if response.status_code == 400:
+        raise gr.Error("Please, fill the empty fields")
+
+    print(response.status_code)
+    out = response.json()
+        
+    return out["answer"], out["score"]
+
+
+with gr.Blocks(theme=gr.themes.Soft()) as demo:
+    gr.Markdown(
+    """
+    # Inference server for Question-Answering
+    Start typing below to see the result.
+    """
+    )
+    with gr.Row() as row:
+        with gr.Column():
+            question_txt = gr.Textbox(label="Type question")
+            context_txt = gr.Textbox(label="Type context for question", lines=2)
+            with gr.Row() as row:
+                btn = gr.Button(value="Submit")
+                clear_btn = gr.ClearButton(value="Clear")
+        with gr.Column():
+            ans_txt = gr.Textbox(value="", label="Answer")
+            score_txt = gr.Textbox(value="", label="Score")
+    btn.click(
+        predict_answer, 
+        inputs=[question_txt, context_txt], 
+        outputs=[ans_txt, score_txt]
+    )
+    clear_btn.click(lambda: [None, None], outputs=[question_txt, context_txt])
+
+if __name__ == "__main__":
+    share_prop = os.getenv("IS_SHAREABLE", False)
+    demo.queue(default_concurrency_limit=10).launch(
+        share=share_prop,
+        server_name="0.0.0.0",
+        server_port=7860,
+    )
